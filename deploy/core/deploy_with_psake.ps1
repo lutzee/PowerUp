f153d20b422f6d0404f3dadc4cc5f403235bab32
Andrey Shchekin <ashmind@gmail.com>
2013-12-04 18:24:55 +1300

    1. Updated PowerUp to correctly fail on psake failure. 2. Changed SettingsParser to support any kind of whitespace separator (not just tabs).

--- deploy/core/deploy_with_psake.ps1
+++ deploy/core/deploy_with_psake.ps1
@@ -1,24 +1,36 @@
 param([string]$deployFile = ".\deploy.ps1", [string]$deploymentProfile, $tasks="default")
 
+$ExitCode = 0;
 try {
-	$ErrorActionPreference='Stop'
+    $ErrorActionPreference='Stop'
 
-	write-host "Deploying package using profile $deploymentProfile"	
-	write-host "Deployment being run under account $env:username"
+    write-host "Deploying package using profile $deploymentProfile"	
+    write-host "Deployment being run under account $env:username"
 
-	write-host "Importing basic modules required by PowerUp"
-	$currentPath = Get-Location
-	$env:PSModulePath = $env:PSModulePath + ";$currentPath\_powerup\deploy\core\" + ";$currentPath\_powerup\deploy\modules\" + ";$currentPath\_powerup\deploy\combos\"
+    write-host "Importing basic modules required by PowerUp"
+    $currentPath = Get-Location
+    $env:PSModulePath = $env:PSModulePath + ";$currentPath\_powerup\deploy\core\" + ";$currentPath\_powerup\deploy\modules\" + ";$currentPath\_powerup\deploy\combos\"
 
-	import-module psake.psm1
-			
-	write-host "Calling psake with deployment file $deployFile "
-	$psake.use_exit_on_error = $true
-	invoke-psake $deployFile $tasks -parameters @{"deployment.profile"=$deploymentProfile} 
+    import-module psake.psm1
+        
+    write-host "Calling psake with deployment file $deployFile "
+    $psake.use_exit_on_error = $true
+    invoke-psake $deployFile $tasks -parameters @{"deployment.profile"=$deploymentProfile}
+
+    if (-not $psake.build_success) {
+        $host.ui.WriteErrorLine("Build Failed!")
+        $ExitCode = 1
+    }
+    else {
+        $ExitCode = $LastExitCode
+    }
 }
 finally {
-try{
-	remove-module psake
-}
-catch{}
+    write-host "Exiting with exit code: $ExitCode"
+    try {
+        remove-module psake
+    }
+    catch{}
+  
+    exit $ExitCode
 }

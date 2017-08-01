978cd446a04b47b1c1db9daec37322672275a003
Paul Suart <paulsuart@gmail.com>
2017-08-01 10:29:03 +0100

    Bumped assembly version; stopped taking version params on cmdline - version is now stored in main.build.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("1.1.0.55")]
-[assembly: AssemblyFileVersionAttribute("1.1.0.55")]
+[assembly: AssemblyVersionAttribute("1.1.0.57")]
+[assembly: AssemblyFileVersionAttribute("1.1.0.57")]
 

9cd8f3d8c064e773a2f0a92b73434224febb397f
Paul Suart <paulsuart@gmail.com>
2018-01-05 10:25:44 +0000

    Updated various plumbing files

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("1.1.0.57")]
-[assembly: AssemblyFileVersionAttribute("1.1.0.57")]
+[assembly: AssemblyVersionAttribute("1.1.0.60")]
+[assembly: AssemblyFileVersionAttribute("1.1.0.60")]
 

92aec9885ed623632e3beeab8b966af6e2f6fbd0
Paul Suart <paulsuart@gmail.com>
2018-01-05 10:51:09 +0000

    Bumped to v1.0.61

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("1.1.0.60")]
-[assembly: AssemblyFileVersionAttribute("1.1.0.60")]
+[assembly: AssemblyVersionAttribute("1.1.0.61")]
+[assembly: AssemblyFileVersionAttribute("1.1.0.61")]
 
